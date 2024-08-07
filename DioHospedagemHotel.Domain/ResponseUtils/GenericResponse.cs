68fd4f16ec00e2b49e13addb7a2a0125da8ca9f9
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-08-07 19:58:26 -0300

    :sparkles: feat: Value Objects Email and Phone

---
+++ DioHospedagemHotel.Domain/ResponseUtils/GenericResponse.cs
@@ -0,0 +1,27 @@
+﻿namespace DioHospedagemHotel.Domain.ResponseUtils
+{
+    public class GenericResponse
+    {
+        public GenericResponse()
+        {
+
+        }
+
+        public List<string> Message { get; private set; }
+        public bool IsValid { get; private set; }
+
+        public void Validate()
+        {
+            if (Message.Count != 0)
+            {
+                IsValid = false;
+            }
+                
+        }
+
+        public void AddMessage(string message)
+        {
+            Message.Add(message);
+        }
+    }
+}
---
+++ DioHospedagemHotel.Domain/ValueObjects/Phone.cs
@@ -0,0 +1,30 @@
+﻿using DioHospedagemHotel.Domain.ResponseUtils;
+
+namespace DioHospedagemHotel.Domain.ValueObjects
+{
+    public class Phone
+    {
+        public Phone(string phone)
+        {
+            PhoneAddress = phone;
+        }
+
+        public string PhoneAddress { get; set; }
+        
+
+        public GenericResponse IsValid()
+        {
+            GenericResponse response = new GenericResponse();
+
+            if (PhoneAddress.Length != 11)
+                response.AddMessage("Telefone deve conter 11 caracteres!");
+
+            if (int.TryParse(PhoneAddress, out var result) == false)
+                response.AddMessage("Apenas numeros são permitidos no campo!");
+
+            response.Validate();
+
+            return response;
+        }
+    }
+}

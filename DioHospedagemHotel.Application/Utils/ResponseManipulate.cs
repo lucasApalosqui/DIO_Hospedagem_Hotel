3b8dc3b6597441e2aca599bda537bddca01d050f
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-08-08 16:28:56 -0300

    :sparkles: feat: Add Program and Menu Screens

---
+++ DioHospedagemHotel.Application/Utils/ResponseManipulate.cs
@@ -0,0 +1,13 @@
+﻿namespace DioHospedagemHotel.Application.Utils
+{
+    public static class ResponseManipulate
+    {
+        public static void SeeErrors(List<string> errors)
+        {
+            foreach (var error in errors)
+            {
+                Console.WriteLine(error);
+            }
+        }
+    }
+}

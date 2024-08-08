d7aede414af6f92aa56a3a1f2b65a2d3d95c7065
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-08-08 16:30:28 -0300

    :sparkles: feat: Reserva Entity and Program first configuration

---
+++ DioHospedagemHotel.Domain/Entities/Reserva.cs
@@ -0,0 +1,70 @@
+﻿using DioHospedagemHotel.Domain.Utils.FilesUtils;
+using DioHospedagemHotel.Domain.Utils.ResponseUtils;
+
+namespace DioHospedagemHotel.Domain.Entities
+{
+    public class Reserva
+    {
+        private readonly HospResponsavel _hospRes;
+        public Reserva(Suite suite, HospResponsavel hospRes, int diasReservados)
+        {
+            Suite = suite;
+            HospedeList.Add(hospRes);
+            DiasReservados = diasReservados;
+            _hospRes = hospRes;
+        }
+
+        public Suite Suite { get; private set; }
+        public List<Hospede> HospedeList { get; private set; } = new List<Hospede>();
+        public int DiasReservados { get; private set; }
+
+        public GenericResponse CadastrarHospedes(List<Hospede> hospConv)
+        {
+            GenericResponse response = new GenericResponse();
+            if (hospConv.Count + 1 > Suite.Capacidade)
+                response.AddMessage("Quantidade de hospedes não pode ultrapassar a capacidade da suite");
+
+            response.Validate();
+
+            if (response.IsValid)
+            {
+                foreach (var hosp in hospConv)
+                {
+                    HospedeList.Add(hosp);
+                }
+            }
+
+            return response;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return HospedeList.Count;
+        }
+
+        public double CalcularValorTotal()
+        {
+            return Suite.ValorDiaria * DiasReservados;
+        }
+
+        public GenericResponse GerarArquivoComDadosDaReserva(string pathFile)
+        {
+            GenericResponse resp = new GenericResponse();
+            string[] linesToAdd = WriteReserveData.WriteReserve(_hospRes, HospedeList, Suite, this);
+            try
+            {
+                File.WriteAllLines(pathFile, linesToAdd);
+                resp.Validate();
+                return resp;
+            }
+            catch (Exception ex)
+            {
+                {
+                    resp.AddMessage("Erro ao gerar arquivo");
+                    resp.Validate();
+                    return resp;
+                }
+            }
+        }
+    }
+}
---
+++ DioHospedagemHotel.Domain/Utils/FilesUtils/WriteReserveData.cs
@@ -0,0 +1,26 @@
+﻿using DioHospedagemHotel.Domain.Entities;
+
+namespace DioHospedagemHotel.Domain.Utils.FilesUtils
+{
+    public static class WriteReserveData
+    {
+        public static string[] WriteReserve(HospResponsavel hospRes, List<Hospede> hospedeList, Suite suite, Reserva reserva)
+        {
+            string[] lines =
+            {
+                $"Dados da Reserva \n",
+                $"Hospede Responsável: {hospRes.Name} {hospRes.LastName} RG: {hospRes.Rg.Mask()} CPF: {hospRes.Cpf.Mask()} Email: {hospRes.Email.EmailAddress} telefone: {hospRes.Phone.PhoneAddress}",
+                $"Hospedes Convidados"
+
+            };
+            foreach (var hospede in hospedeList.OfType<HospConvidado>().ToList())
+            {
+                lines.Append($"Nome Completo: {hospede.Name} {hospede.LastName} RG: {hospede.Rg}");
+            }
+
+            lines.Append($"Cód Suite: {suite.Id} - Tipo: {suite.Tipo.ToString()} - Capacidade Máxima: {suite.Capacidade} - Valor Diária: {suite.ValorDiaria.ToString("C")} - Dias Reservados: {reserva.DiasReservados} - Valor Total: {reserva.CalcularValorTotal().ToString("C")}");
+
+            return lines;
+        }
+    }
+}

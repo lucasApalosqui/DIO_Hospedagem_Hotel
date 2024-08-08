13836f2683339e71ac642abaa20831013ddf9e3e
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-08-08 19:11:20 -0300

    :recycle: refactor: Refactor Guid Gen

--- DioHospedagemHotel.Domain/Entities/Suite.cs
+++ DioHospedagemHotel.Domain/Entities/Suite.cs
@@ -6,12 +6,13 @@ namespace DioHospedagemHotel.Domain.Entities
     {
         public Suite(TipoSuite suite, int capacidade, double valorDiaria)
         {
+            Id = Guid.NewGuid();
             Tipo = suite;
             Capacidade = capacidade;
             ValorDiaria = valorDiaria;
         }
 
-        public Guid Id { get; set; } = new Guid();
+        public Guid Id { get; set; }
         public TipoSuite Tipo {  get; private set; }
         public int Capacidade { get; private set; }
         public double ValorDiaria {  get; private set; }

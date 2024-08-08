b90d53ab981354b1a23ab5ac7cf40f68dea2825c
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-08-08 19:12:12 -0300

    :recycle: refactor: Refactor Guid Gen Hospede

--- DioHospedagemHotel.Domain/Entities/Hospede.cs
+++ DioHospedagemHotel.Domain/Entities/Hospede.cs
@@ -5,7 +5,7 @@ namespace DioHospedagemHotel.Domain.Entities
 {
     public abstract class Hospede
     {
-        public Guid NumIdentity { get; } = new Guid();
+        public Guid NumIdentity { get; } = Guid.NewGuid();
         public Rg Rg { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }

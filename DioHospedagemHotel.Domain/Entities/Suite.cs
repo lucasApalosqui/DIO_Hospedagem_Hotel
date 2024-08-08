f370c17f17f26bdb4f40f836ba163cada4e33d9d
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-08-08 11:05:16 -0300

    :sparkles: feat: TipoSuite Enum and Suite Entitie

---
+++ DioHospedagemHotel.Domain/Entities/Suite.cs
@@ -0,0 +1,21 @@
+﻿using DioHospedagemHotel.Domain.Enums;
+
+namespace DioHospedagemHotel.Domain.Entities
+{
+    public class Suite
+    {
+        public Suite(TipoSuite suite, int capacidade, double valorDiaria)
+        {
+            Tipo = suite;
+            Capacidade = capacidade;
+            ValorDiaria = valorDiaria;
+        }
+
+        public Guid Id { get; set; } = new Guid();
+        public TipoSuite Tipo {  get; private set; }
+        public int Capacidade { get; private set; }
+        public double ValorDiaria {  get; private set; }
+
+
+    }
+}
---
+++ DioHospedagemHotel.Domain/Enums/TipoSuite.cs
@@ -0,0 +1,10 @@
+﻿namespace DioHospedagemHotel.Domain.Enums
+{
+    public enum TipoSuite
+    {
+        Solo = 1,
+        Casal = 2,
+        Familia = 3,
+        Premium = 4
+    }
+}
